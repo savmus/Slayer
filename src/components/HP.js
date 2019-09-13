2a22259566bad364d31e8142fc50689a80359123
Savannah <savannahmusladin@yahoo.com>
2019-09-13 12:56:31 -0700

    Styling

--- src/components/HP.js
+++ src/components/HP.js
@@ -8,6 +8,7 @@ function HP(canvas, player) {
     this.player = player;
     this.health = new Health(140, 15, 440, 20, this.ctx, this.player);
     this.hp = new Text("14px", "Sans-Serif", "black", 440, 15, this.ctx, "100/100 HP");
+    this.weapon = new Text("14px", "Sans-Serif", "black", 440, 50, this.ctx, "Weapon: ");
 
     this.update = this.update.bind(this);
 
@@ -20,9 +21,13 @@ HP.prototype.clear = function clear() {
 
 HP.prototype.update = function update() {
     this.clear();
+
     this.hp.text = this.player.health + "/100 HP";
+    this.weapon.text = "Weapon: " + this.player.weapon;
+
     this.health.update();
     this.hp.update();
+    this.weapon.update();
 }
 
 module.exports = HP;
